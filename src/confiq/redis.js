0d6ac96230c87ca6016747aa57f8bc2a1b3bd327
Jitsu <sujitku641@gmail.com>
2026-04-07 09:05:21 +0530

    resolve merge conflict

--- src/confiq/redis.js
+++ src/confiq/redis.js
@@ -4,10 +4,9 @@ const redisclient = createClient({
     username: 'default',
     password: process.env.Redis_Pass,
     socket: {
-        host: 'redis-19816.c278.us-east-1-4.ec2.redns.redis-cloud.com',
-        port: 19816
+        host: 'redis-12592.c15.us-east-1-4.ec2.cloud.redislabs.com',
+        port: 12592
     }
-
 });
 
 module.exports=redisclient;
--- src/index.js
+++ src/index.js
@@ -10,24 +10,20 @@ const problemRouter=require("../src/routes/problemCreator")
 const submitRouter=require("../src/routes/submit")
 const cors=require('cors');
 const courseRouter = require("./routes/courseRouter");
-
 const GoodiesRouter=require("./routes/goodiesRoutes")
 const getvideotoken =require("./controllers/videocall")
-
 const premiumrouter=require("./routes/premiumrouter")
-
 const server = http.createServer(app);
+
 app.use(cors({
   origin: 'https://codelikho.vercel.app',
   credentials:true
 }))
 
-//socket.io is setup kiye kya humlog ek normal http server banaye phir usko socket.io wale server se upgrade kr diye abb koi dikkat nhi hoga io ke andar mera abb socket wala server hai mtlb jo bhi server socket wala kaam hoga wo io se hoga !
-
 const { Server } = require('socket.io');
 const io = new Server(server, {
   cors: {
-    origin:  'https://codelikho.vercel.app',
+    origin: 'https://codelikho.vercel.app',
     methods: ['GET', 'POST'],
     credentials: true
   }
@@ -36,24 +32,20 @@ const io = new Server(server, {
 const chatHandler = require('./utils/Socket');
 chatHandler(io);
 
-
-
-
 app.use(express.json())
 app.use(cookieParser())
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'ok', uptime: process.uptime() })
+})
 
 app.use("/user",authRouter)
 app.use("/problem",problemRouter)
 app.use("/submission",submitRouter)
 app.use("/course",courseRouter)
-
 app.use("/goodies",GoodiesRouter)
 app.get('/agoratoken/:id',getvideotoken)
-
 app.use("/premium",premiumrouter)
-app.get('/health', (req, res) => {
-  res.json({ status: 'ok' })
-})
 
 const InitializeConnection=async()=>{
   try{
@@ -61,12 +53,9 @@ const InitializeConnection=async()=>{
     console.log("DB connected !")
     server.listen(process.env.PORT,()=>{
      console.log("server is listening !"+process.env.PORT);})
-
   }
   catch(err){
     console.log("Error"+err)
   }
 }
 InitializeConnection();
-
-
